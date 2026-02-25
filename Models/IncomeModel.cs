dd295fdace02b4ab8718e10cd2c354fc13dba265
Арсений Сеньков <senkxv@gmail.com>
2026-02-25 17:59:10 +0300

    intermediate commit

--- Models/IncomeModel.cs
+++ Models/IncomeModel.cs
@@ -6,6 +6,6 @@
         public decimal Amount { get; set; } = 0;
         public DateTime Date { get; set; }
         public string? Wallet { get; set; }
-        public string? Comment { get; set; }
+        public string? Comment { get; set; } = "коммент";
     }
 }
